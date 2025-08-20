6764835fca34284eb756d76df06ead0e5f79f7bf
sbickel3 <sbickel201@gmail.com>
2025-08-20 05:21:47 -0400

    using default logout functionality

--- src/main/java/com/spring/security/config/SecurityConfig.java
+++ src/main/java/com/spring/security/config/SecurityConfig.java
@@ -2,16 +2,11 @@ package com.spring.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
-
-import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -44,23 +39,12 @@ public class SecurityConfig {
                 .requestMatchers("/welcome").permitAll()
                 .anyRequest().authenticated())
             .formLogin(form -> form
-            	        .defaultSuccessUrl("/welcome") 
+            	        .defaultSuccessUrl("/welcome")
             	        .permitAll())
-            .logout((logout) -> logout
-            		.logoutRequestMatcher(PathPatternRequestMatcher.withDefaults().matcher(HttpMethod.GET, "/logout"))
-            		.logoutSuccessHandler(logoutSuccessHandler())
-            		.permitAll())
+            .logout(Customizer.withDefaults())
             .httpBasic(Customizer.withDefaults());
 
         return http.build();
-    }
-    
-    private LogoutSuccessHandler logoutSuccessHandler() {
-        return (request, response, authentication) -> {
-            response.setStatus(HttpServletResponse.SC_OK);
-            response.setContentType("application/json");
-            response.getWriter().write("{\"message\": \"You have been logged out successfully\"}");
-        };
     }
 }
 
